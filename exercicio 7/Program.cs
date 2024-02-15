401cd37514ac8b553e2d153839afc3065bb2102a
AlexandreBNE <160055836+AlexandreBNE@users.noreply.github.com>
2024-02-15 14:38:08 -0300

    Add files via upload

---
+++ exercicio 7/Program.cs
@@ -0,0 +1,62 @@
+﻿﻿using System;
+
+class Investment
+{
+    private double initialValue;
+    private double monthlyInterest;
+
+    public Investment(double initialValue, double monthlyInterest)
+    {
+        this.initialValue = initialValue;
+        this.monthlyInterest = monthlyInterest;
+    }
+
+    public double calculateFutureIncome(int month, double withdraw)
+    {
+        double futureIncome = initialValue;
+
+        for (int i = 1; i <= month; i++)
+        {
+            if (i == 5)
+            {
+                futureIncome -= withdraw;
+            }
+            else
+            {
+                futureIncome *= (1 + monthlyInterest);
+            }
+        }
+
+        return futureIncome;
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        System.Console.Write("Digite o valor inicial: ");
+        double initialValue = double.Parse(Console.ReadLine());
+
+        System.Console.Write("Digite a taxa de juros mensal (%): ");
+        double monthlyInterest = double.Parse(Console.ReadLine()) / 100;
+
+        System.Console.Write("No 5º mês terá um resgate, digite o valor do resgate: ");
+        double withdraw = double.Parse(Console.ReadLine());
+
+        Investment Investment = new Investment(initialValue, monthlyInterest);
+
+        System.Console.WriteLine("Mês| Rendimento");
+        System.Console.WriteLine("---|---------------");
+
+        for (int i = 1; i <= 8; i++)
+        {
+            double rendimentoMensal = Investment.calculateFutureIncome(i, withdraw);
+            System.Console.WriteLine($"{i}  | {rendimentoMensal:C}");
+        }
+
+        double totalIncome = Investment.calculateFutureIncome(8, withdraw);
+        System.Console.WriteLine($"Rendimento total: {totalIncome:C}");
+    }
+}
---
+++ exercicio 8/Program.cs
@@ -0,0 +1,62 @@
+﻿﻿using System;
+
+class Investment
+{
+    private double initialValue;
+    private double monthlyInterest;
+
+    public Investment(double initialValue, double monthlyInterest)
+    {
+        this.initialValue = initialValue;
+        this.monthlyInterest = monthlyInterest;
+    }
+
+    public double CalculateFutureIncome(int month, double withdraw)
+    {
+        double futureIncome = initialValue;
+
+        for (int i = 1; i <= month; i++)
+        {
+            if (i == 5)
+            {
+                futureIncome -= withdraw;
+            }
+            else
+            {
+                futureIncome *= (1 + monthlyInterest);
+            }
+        }
+
+        return futureIncome;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+
+        System.Console.Write("Digite o valor inicial: ");
+        double initialValue = double.Parse(Console.ReadLine());
+
+        System.Console.Write("Digite a taxa de juros mensal (%): ");
+        double monthlyInterest = double.Parse(Console.ReadLine()) / 100;
+
+        System.Console.Write("No 5º mês terá um resgate, digite o valor do resgate: ");
+        double withdraw = double.Parse(Console.ReadLine());
+
+        Investment investment = new Investment(initialValue, monthlyInterest);
+
+        System.Console.WriteLine("\nMês | Valor Inicial | (%) |  Resgate  | Saldo Final");
+        System.Console.WriteLine("----|---------------|-----|-----------|------------");
+
+        for (int i = 1; i <= 8; i++)
+        {
+            double monthlyIncome = investment.CalculateFutureIncome(i, withdraw);
+            System.Console.WriteLine($"{i,-4}| {initialValue:C2}   | {monthlyInterest * 100}%  | {(i == 5 ? withdraw.ToString("C") : "    -    ")} | {monthlyIncome:C}");
+        }
+
+        double totalIncome = investment.CalculateFutureIncome(8, withdraw);
+        System.Console.WriteLine($"\nRendimento final: {totalIncome:C}");
+    }
+}
